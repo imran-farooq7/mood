d3f8f74e2688fec31595f2d6130536d5dc05070b
imran farooq <imran_farooq7@hotmail.com>
2023-07-18 19:19:22 +0500

    Sign in and sign up css updated to dark theme

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,9 @@
 import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
-	return <SignIn />;
+	return (
+		<div className="w-screen h-screen bg-black flex justify-center items-center text-white">
+			<SignIn />
+		</div>
+	);
 }
--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,9 @@
 import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
-	return <SignUp redirectUrl="/new-user" />;
+	return (
+		<div className="w-screen h-screen bg-black flex justify-center items-center text-white">
+			<SignUp redirectUrl="/new-user" />
+		</div>
+	);
 }
